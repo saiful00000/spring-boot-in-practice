24d80d4e1ee78e3d5ae8c0460bd00d48bb6f8b2b
saiful00000 <saiful161003@gmail.com>
2023-02-14 00:30:40 +0600

    added fiest logging level to the application.properties file

--- src/main/java/com/shaiful/demo/DemoApplication.java
+++ src/main/java/com/shaiful/demo/DemoApplication.java
@@ -1,13 +1,11 @@
 package com.shaiful.demo;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shaiful.demo.models.Employee;
-import org.apache.coyote.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,22 +17,26 @@ import java.util.*;
 @RestController
 public class DemoApplication {
 	public static void main(String[] args) {
+		System.out.println("\n\nEntering to the run() method to start the application.......\n\n");
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
 
-	@GetMapping("/get-data")
+	@GetMapping("/get-employee-list")
 	@ResponseBody
-	public JsonNode getData() throws JsonProcessingException {
+	public JsonNode getEmployeeList() throws JsonProcessingException {
 		List<Employee> employeeList = new ArrayList<>();
-		for(int i = 0; i < 3; i++){
-			employeeList.add(new Employee("Name" + i, "SW", 55000, "E2", 5.7));
+		for (int i = 0; i < 3; i++) {
+			String name = "Name " + i;
+			employeeList.add(new Employee(name, "SW", 55000, "E2", 5.7));
 		}
 
 		ObjectMapper mapper = new ObjectMapper();
-		return  mapper.readTree(mapper.writeValueAsString(employeeList));
+		return mapper.readTree(mapper.writeValueAsString(employeeList));
 	}
+
 }
